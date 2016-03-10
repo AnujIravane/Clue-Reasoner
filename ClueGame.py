00737bc9a9d2c76959d13950c449d32f6336f74b
Anuj Iravane <anudge7@gmail.com>
2016-03-10 06:06:39 -0600

    We done gg

--- ClueGame.py
+++ ClueGame.py
@@ -7,37 +7,86 @@ suspects = ["mustard", "plum", "green", "peacock", "scarlett", "white"]
 weapons = ["knife", "candlestick", "revolver", "rope", "pipe", "wrench"]
 rooms = ["hall", "lobby", "dining", "kitchen", "ballroom", "conservatory", "billiard", "library", "study"]
 cards = suspects + weapons + rooms
+numPlayers = 6
 # Initialize important variables
 class ClueGame:
-    
-    
-    caseFile.append(players[random.randInt(0,len(players)-1)])
-    caseFile.append(suspects[random.randInt(0,len(suspects)-1)])
-    caseFile.append(weapons[random.randInt(0,len(weapons)-1)])
-    
-    del cards[cards.index(caseFile[0])]
-    del cards[cards.index(caseFile[1])]
-    del cards[cards.index(caseFile[2])]
-    
-    numPlayers = 6
-    
     def __init__(self):
         # nothing to see here. Move along
         self.playerReasoners = []
         self.playerTurn = 0
         random.shuffle(cards)
         self.hands = {}
-        for i in xrange(0, numPlayers-1):
-            hands[i] = []
+
+        caseFile.append(players[random.randint(0,len(players)-1)])
+        caseFile.append(weapons[random.randint(0,len(weapons)-1)])
+        caseFile.append(rooms[random.randint(0,len(suspects)-1)])
+        print caseFile
+
+        del cards[cards.index(caseFile[0])]
+        del cards[cards.index(caseFile[1])]
+        del cards[cards.index(caseFile[2])]
+
+        for i in xrange(0, numPlayers):
+            self.hands[i] = []
             
-        for j in xrange(0,len(cards)-1):
-            hands[j%numPlayers].append(cards[j])
+        for j in xrange(0,len(cards)):
+            self.hands[j%numPlayers].append(cards[j])
            
-        for k in xrange(0, numPlayers-1):
-            playerReasoners.append(CluePlayer.CluePlayer(players[k],hand[k],"reasoner"))
+        for k in xrange(0, numPlayers):
+            self.playerReasoners.append(CluePlayer.CluePlayer(players[k],self.hands[k],"reasoner"))
+
+    def startGame(self):
+        while (self.playNextTurn()):
+            self.playerTurn = (self.playerTurn + 1)%numPlayers
     
     def playNextTurn(self):
-    	
+        guess = self.playerReasoners[self.playerTurn].makeMove()
+        index = (self.playerTurn + 1) % numPlayers
+        if (guess[-1] == "s"):
+            refuteResult = None
+            didRefute = False
+            while (index != self.playerTurn):
+                refuteResult = self.playerReasoners[index].refute(guess[0:3])
+                if not(refuteResult is None):
+                    didRefute = True
+                    break
+                index = (index + 1) % numPlayers
+            if not(refuteResult is None):
+                print self.playerReasoners[self.playerTurn].name + " SUGGESTS " + guess[0] + " killed with a " + guess[1] + " in the " + guess[2] + ". Refuted by " + self.playerReasoners[index].name + " with card " + refuteResult
+            else:
+                print self.playerReasoners[self.playerTurn].name + " SUGGESTS " + guess[0] + " killed with a " + guess[1] + " in the " + guess[2] + ". Not refuted."
+            for j in range(0,numPlayers):
+                if not(refuteResult is None):
+                    if (j==self.playerTurn):
+                        self.playerReasoners[j].suggest(self.playerReasoners[self.playerTurn].name,guess[0],guess[1],guess[2],self.playerReasoners[index].name,refuteResult)
+                    else:
+                        self.playerReasoners[j].suggest(self.playerReasoners[self.playerTurn].name,guess[0],guess[1],guess[2],self.playerReasoners[index].name,None)
+                else:
+                    self.playerReasoners[j].suggest(self.playerReasoners[self.playerTurn].name,guess[0],guess[1],guess[2],None,None)
+        else:
+            isCorrect = True
+            for accusedCards in guess[0:-1]:
+                if not(accusedCards in caseFile):
+                    isCorrect = False
+            for j in range(0,len(self.playerReasoners)):
+                self.playerReasoners[j].accuse(self.playerReasoners[self.playerTurn].name,guess[0],guess[1],guess[2],isCorrect)
+
+            if isCorrect:
+                print self.playerReasoners[self.playerTurn].name + " ACCUSES " + guess[0] + " killed with a " + guess[1] + " in the " + guess[2] + ". CORRECT GUESS"
+                print "Game Over"
+                return False
+            else:
+                print self.playerReasoners[self.playerTurn].name + " ACCUSES " + guess[0] + " killed with a " + guess[1] + " in the " + guess[2] + ". WRONG GUESS"
+        return True
+
+
+
+                
+
+
+
+
+
 
     
     
--- CluePlayer.py
+++ CluePlayer.py
@@ -12,232 +12,229 @@ weapons = ["knife", "candlestick", "revolver", "rope", "pipe", "wrench"]
 rooms = ["hall", "lobby", "dining", "kitchen", "ballroom", "conservatory", "billiard", "library", "study"]
 cards = suspects + weapons + rooms
 
-class CluePlayer
+class CluePlayer:
         
     def __init__(self,name,hand,type):
         # do nothing
-        self.hand = hand
+        self.playerHand = hand
         self.name = name
         self.type = type
-        self.clauses = initialClauses()
-        self.clauses.append(hand(hand))
+        self.clauses = self.iClauses()
+        self.clauses.extend(self.hand(self.name,hand))
+        print self.playerHand
         
     def makeMove(self):
         suspects = ClueGame.suspects
         weapons = ClueGame.weapons
         rooms = ClueGame.rooms
         players = ClueGame.players
-        if (type == "reasoner"):
-            guess = self.makeMoveReasoner()       
+        if (self.type == "reasoner"):
+            guess = self.makeMoveReasoner(self.clauses,self.playerHand) 
+            return guess
+        return None
 
     # Initialize important variables
 
-    def getPairNumFromNames(player,card):
-        return getPairNumFromPositions(locations.index(player),
+    def getPairNumFromNames(self,player,card):
+        return self.getPairNumFromPositions(locations.index(player),
                                        cards.index(card))
     
-    def getPairNumFromPositions(player,card):
+    def getPairNumFromPositions(self,player,card):
         return player*len(cards) + card + 1
     
     
-    def initialClauses():
+    def iClauses(self):
         global players, locations, weapons, rooms, cards, suspects, caseFile
     
         clauses = []
     
         # Each card is in at least one place (including case file).
         for c in cards:
-            clauses.append([getPairNumFromNames(p,c) for p in locations])
+            clauses.append([self.getPairNumFromNames(p,c) for p in locations])
     
         # A card cannot be in two places.
         for card in cards:
             for l1 in locations:
                 for l2 in locations:
                     if l1 != l2:
-                        clauses.append([(-1)*getPairNumFromNames(l1,card),(-1)*getPairNumFromNames(l2,card)])
+                        clauses.append([(-1)*self.getPairNumFromNames(l1,card),(-1)*self.getPairNumFromNames(l2,card)])
     
     
         # At least one card of each category is in the case file.
-            clauses.append([getPairNumFromNames(caseFile,w) for w in weapons])
-            clauses.append([getPairNumFromNames(caseFile,r) for r in rooms])
-            clauses.append([getPairNumFromNames(caseFile,p) for p in suspects])
+            clauses.append([self.getPairNumFromNames(caseFile,w) for w in weapons])
+            clauses.append([self.getPairNumFromNames(caseFile,r) for r in rooms])
+            clauses.append([self.getPairNumFromNames(caseFile,p) for p in suspects])
     
         # No two cards in each category can both be in the case file.
         for w1 in weapons:
             for w2 in weapons:
                 if w1 != w2:
-                    clauses.append([(-1)*getPairNumFromNames(caseFile,w1), (-1)*getPairNumFromNames(caseFile,w2)])
+                    clauses.append([(-1)*self.getPairNumFromNames(caseFile,w1), (-1)*self.getPairNumFromNames(caseFile,w2)])
     
         for w1 in rooms:
             for w2 in rooms:
                 if w1 != w2:
-                    clauses.append([(-1)*getPairNumFromNames(caseFile,w1), (-1)*getPairNumFromNames(caseFile,w2)])
+                    clauses.append([(-1)*self.getPairNumFromNames(caseFile,w1), (-1)*self.getPairNumFromNames(caseFile,w2)])
     
         for w1 in suspects:
             for w2 in suspects:
                 if w1 != w2:
-                    clauses.append([(-1)*getPairNumFromNames(caseFile,w1), (-1)*getPairNumFromNames(caseFile,w2)])
+                    clauses.append([(-1)*self.getPairNumFromNames(caseFile,w1), (-1)*self.getPairNumFromNames(caseFile,w2)])
     
         return clauses
-    # end initialClauses
+    # end initialClausess
     
     
-    def hand(player,cards):
+    def hand(self,player,cards):
         clauses = []
         for card in cards:
-            clauses.append([getPairNumFromNames(player,card)])
+            clauses.append([self.getPairNumFromNames(player,card)])
         return clauses
     # end hand
     
     
-    def suggest(suggester,card1,card2,card3,refuter,cardShown):
+    def suggest(self,suggester,card1,card2,card3,refuter,cardShown):
         global players
-    
+        #print refuter
+        #print cardShown
         clauses = []
         if refuter is None:
             for location in players:
                 if location != suggester:
-                    clauses.append([(-1)*getPairNumFromNames(location,card1)])
-                    clauses.append([(-1)*getPairNumFromNames(location,card2)])
-                    clauses.append([(-1)*getPairNumFromNames(location,card3)])
+                    clauses.append([(-1)*self.getPairNumFromNames(location,card1)])
+                    clauses.append([(-1)*self.getPairNumFromNames(location,card2)])
+                    clauses.append([(-1)*self.getPairNumFromNames(location,card3)])
         else:
             if not(cardShown is None):
-                clauses.append([getPairNumFromNames(refuter,cardShown)])
+                clauses.append([self.getPairNumFromNames(refuter,cardShown)])
             else:
-                clauses.append([getPairNumFromNames(refuter,card1),getPairNumFromNames(refuter,card2),getPairNumFromNames(refuter,card3)])
+                clauses.append([self.getPairNumFromNames(refuter,card1),self.getPairNumFromNames(refuter,card2),self.getPairNumFromNames(refuter,card3)])
     
             suggesterIndex = players.index(suggester) + 1
             suggesterIndex %= len(players)
             while(players[suggesterIndex] != refuter):
-                clauses.append([(-1)*getPairNumFromNames(players[suggesterIndex],card1)])
-                clauses.append([(-1)*getPairNumFromNames(players[suggesterIndex],card2)])
-                clauses.append([(-1)*getPairNumFromNames(players[suggesterIndex],card3)])
+                clauses.append([(-1)*self.getPairNumFromNames(players[suggesterIndex],card1)])
+                clauses.append([(-1)*self.getPairNumFromNames(players[suggesterIndex],card2)])
+                clauses.append([(-1)*self.getPairNumFromNames(players[suggesterIndex],card3)])
                 suggesterIndex += 1 
                 suggesterIndex %= len(players)
     
-        return clauses
+        self.clauses.extend(clauses)
     # end suggest
     
     
     
     
-    def accuse(accuser,card1,card2,card3,isCorrect):
+    def accuse(self,accuser,card1,card2,card3,isCorrect):
         global caseFile
     
         clauses = []
-        clauses.append([(-1)*getPairNumFromNames(accuser,card1)])
-        clauses.append([(-1)*getPairNumFromNames(accuser,card2)])
-        clauses.append([(-1)*getPairNumFromNames(accuser,card3)])
+        clauses.append([(-1)*self.getPairNumFromNames(accuser,card1)])
+        clauses.append([(-1)*self.getPairNumFromNames(accuser,card2)])
+        clauses.append([(-1)*self.getPairNumFromNames(accuser,card3)])
         if isCorrect:
-            clauses.append([getPairNumFromNames(caseFile,card1)])
-            clauses.append([getPairNumFromNames(caseFile,card2)])
-            clauses.append([getPairNumFromNames(caseFile,card3)])
+            clauses.append([self.getPairNumFromNames(caseFile,card1)])
+            clauses.append([self.getPairNumFromNames(caseFile,card2)])
+            clauses.append([self.getPairNumFromNames(caseFile,card3)])
         else:
-            clauses.append([(-1)*getPairNumFromNames(caseFile,card1),(-1)*getPairNumFromNames(caseFile,card2),(-1)*getPairNumFromNames(caseFile,card3)])
+            clauses.append([(-1)*self.getPairNumFromNames(caseFile,card1),(-1)*self.getPairNumFromNames(caseFile,card2),(-1)*self.getPairNumFromNames(caseFile,card3)])
     
-        return clauses
+        self.clauses.extend(clauses)
     # end accuse
 
-    def refute(cards):
-        temp = [0,1,2]
+    def refute(self,rcards):
+        temp = range(0,len(self.playerHand))
         random.shuffle(temp)
         for i in temp:
-            for card in cards:
-                if (query(hand[i]) == card):
+            for card in rcards:
+                if (self.playerHand[i] == card):
                     return card
         return None
 
     
-    def makeMoveReasoner(clauses,hand):
+    def makeMoveReasoner(self,clauses,hand):
         global players, suspects, weapons, rooms, locations, caseFile
     
         guess = []
-    
-        hasCard = False
-        addedSuspectCard = True
+        accuseGuess = []
+        knowSuspect = False
+        knowWeapon = False
+        knowRoom = False
         for pcard in suspects:
-            for player in locations:
-                if (query(player,pcard,clauses)):
-                    hasCard = True
-                    break
-            if (not hasCard):
-                guess.append(pcard)
-                addedSuspectCard = True;
+            if (self.query(caseFile,pcard,clauses)):
+                accuseGuess.append(pcard)
+                knowSuspect = True
                 break
-            else:
-                hasCard = False
+        for wcard in weapons:
+            if (self.query(caseFile,wcard,clauses)):
+                accuseGuess.append(wcard)
+                knowWeapon = True
+                break
+        for rcard in rooms:
+            if (self.query(caseFile,rcard,clauses)):
+                accuseGuess.append(rcard)
+                knowRoom = True
+                break
+        if (knowSuspect and knowWeapon and knowRoom):
+            accuseGuess.append('a')
+            return accuseGuess
+        
+    
+        
+        addedSuspectCard = False
+        randomSuspects = suspects
+        random.shuffle(randomSuspects)
+        for pcard in randomSuspects:
+            if (self.query(caseFile,pcard,clauses) is None):
+                guess.append(pcard)
+                addedSuspectCard = True
+                break
+        if (not addedSuspectCard):
+            guess.append(suspects[random.randint(0,len(suspects)-1)])       
     
             
         
-        hasCard = False
         addedWeaponCard = False
-        for wcard in weapons:
-            for player in locations:
-                if (query(player,wcard,clauses)):
-                    hasCard = True
-                    break
-            if (not hasCard):
+        randomWeapons = weapons
+        random.shuffle(randomWeapons)
+        for wcard in randomWeapons:
+            if (self.query(caseFile,wcard,clauses)):
                 guess.append(wcard)
                 addedWeaponCard = True
                 break
-            else:
-                hasCard = False
-                
-        
+        if (not addedWeaponCard):
+            guess.append(weapons[random.randint(0,len(weapons)-1)])        
+
     
-        hasCard = False
         addedRoomCard = False
-        for rcard in rooms:
-            for player in locations:
-                if (query(player,rcard,clauses)):
-                    hasCard = True
-                    break
-            if (not hasCard):
+        randomRooms = rooms
+        random.shuffle(randomRooms)
+        for rcard in randomRooms:
+            if (self.query(caseFile,rcard,clauses)):
                 guess.append(rcard)
                 addedRoomCard = True
                 break
-            else:
-                hasCard = False
-        
-    
-    
-        if ((not addedRoomCard) and (not addedWeaponCard) and (not addedSuspectCard)):
-            for pcard in players:
-                if (query(caseFile,pcard,clauses):
-                    guess.append(pcard)
-            for wcard in weapons:
-                if (query(caseFile,wcard,clauses):
-                    guess.append(wcard)
-            for rcard in rooms:
-                if (query(caseFile,rcard,clauses):
-                    guess.append(rcard)
-            guess.append("a")
-        else:
-            if (not addedSuspectCard):
-                guess.append(suspects[random.randInt(0,len(suspects)-1)])
-            if (not addedWeaponCard):
-                guess.append(weapons[random.randInt(0,len(weapons)-1)])
-            if (not addedRoomCard):
-                guess.append(rooms[random.randInt(0,len(rooms)-1)])
-            guess.append("s") 
+        if (not addedRoomCard):
+            guess.append(rooms[random.randint(0,len(rooms)-1)])
+        guess.append("s") 
         return guess
     # end makeMove
     
     
-    def query(player,card,clauses):
-        return SATSolver.testLiteral(getPairNumFromNames(player,card),clauses)
-    # end query
+    def query(self,player,card,clauses):
+        return SATSolver.testLiteral(self.getPairNumFromNames(player,card),clauses)
+    # end self.query
 
-    def queryString(returnCode):
+    def queryString(self,returnCode):
         if returnCode == True:
             return 'Y'
         elif returnCode == False:
             return 'N'
         else:
             return '-'
-    # end queryString
+    # end self.queryString
 
-    def printNotepad(clauses):
+    def printNotepad(self,clauses):
         global players, caseFile, cards
     
         for player in players:
@@ -246,6 +243,6 @@ class CluePlayer
         for card in cards:
             print card[:6],'\t',
             for player in players:
-                print queryString(query(player,card,clauses)),'\t',
-            print queryString(query(caseFile,card,clauses))
+                print self.queryString(self.query(player,card,clauses)),'\t',
+            print self.queryString(self.query(caseFile,card,clauses))
     # end printNotepad
